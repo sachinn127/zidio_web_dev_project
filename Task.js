061fbf055b922b67650aa68e1fb47e224b6d3d52
sachin <sachinpyara7@gmail.com>
2024-12-27 14:18:37 +0530

    Task.js

---
+++ Task.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  priority: {
+    type: String,
+    enum: ['Low', 'Medium', 'High'],
+    default: 'Medium'
+  },
+  status: {
+    type: String,
+    enum: ['Not Started', 'In Progress', 'Completed'],
+    default: 'Not Started'
+  },
+  dueDate: {
+    type: Date
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
